9a9fdce68432d781c41161093b071fc736d7adf2
yukapril <i@yukapril.com>
2018-02-23 16:54:18 +0800

    add homepage

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const request = require('request')
+const fs = require('fs')
 const Koa = require('koa')
 const app = new Koa()
 
-const getGist = (url) => {
+const readFile = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(data)
+        })
+    })
+}
+
+const getGist = url => {
     return new Promise((resolve, reject) => {
         request({
             method: 'GET',
@@ -18,18 +31,27 @@ const getGist = (url) => {
 }
 
 app.use(async ctx => {
-    const prefix = 'https://gist.github.com'
-    // const prefix = 'https://gist.coding.net'
-    const url = prefix + ctx.request.url
-    try {
-        const { response, body } = await getGist(url)
-        ctx.status = response.statusCode
-        ctx.set('content-type', response.headers['content-type'])
-        ctx.body = body
-    } catch (err) {
-        console.log(err)
-        ctx.body = ''
+    const reqUrl = ctx.request.url
+    if (reqUrl === '' || reqUrl === '/') {
+        // homepage
+        ctx.set('content-type', 'text/html; charset=utf-8')
+        ctx.body = await readFile('./index.html')
+    } else {
+        // proxy
+        const prefix = 'https://gist.github.com'
+        // const prefix = 'https://gist.coding.net'
+        const url = prefix + reqUrl
+        try {
+            const { response, body } = await getGist(url)
+            ctx.status = response.statusCode
+            ctx.set('content-type', response.headers['content-type'])
+            ctx.body = body
+        } catch (err) {
+            console.log(err)
+            ctx.body = ''
+        }
     }
+
 })
 
 app.listen(5001)
